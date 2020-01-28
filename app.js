aba76b7facab650673aa379e40579acb130f1ae8
Moses Palacios <mospalacios@gmail.com>
2020-01-28 00:53:20 -0500

    created inqiurer promt that repeatedly asks questions until told to stop

--- app.js
+++ app.js
@@ -1,17 +1,52 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-inquirer
-.prompt([
-    {
-        type: "input",
-        message: "What is your name?",
-        name: "name"
-    },
-    {
-        type: "list",
-        message: "What type of employee would you like to add?",
-        name: "employeeType",
-        choices: ["Manager", "Engineer", "Intern", "None, I'm done!"]
-    }
-]);
+
+const collectInputs = async (inputs = []) => {
+
+    const prompts = [
+        {
+            type: "input",
+            message: "What is your name?",
+            name: "name"
+        },
+        {
+            type: "list",
+            message: "What type of employee would you like to add?",
+            name: "employeeType",
+            choices: ["Manager", "Engineer", "Intern"]
+        },
+        {
+            type: "confirm",
+            message: "Enter another employee?",
+            name: "again",
+            default: true
+        }
+    ];
+
+    const {again, ...answers} = await inquirer.prompt(prompts);
+    const newInputs = [...inputs, answers];
+    return again ? collectInputs(newInputs) : newInputs;
+}
+    
+const main = async () =>{
+    const inputs = await collectInputs();
+    console.log(inputs)
+}
+
+main();
+
+// inquirer
+// .prompt([
+//     {
+//         type: "input",
+//         message: "What is your name?",
+//         name: "name"
+//     },
+//     {
+//         type: "list",
+//         message: "What type of employee would you like to add?",
+//         name: "employeeType",
+//         choices: ["Manager", "Engineer", "Intern", "None, I'm done!"]
+//     }
+// ]);
